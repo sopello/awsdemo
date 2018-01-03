45e44cadd94399f7cfed344f982427057ec6dc95
Marek Berkan <mberkan@e-point.pl>
2018-01-03 16:37:59 +0100

    Pierwsza wersja ze stroną 404.

---
+++ src/main/java/pl/mberkan/softwarecraftmanship/awsdemo/AwsdemoApplication.java
@@ -0,0 +1,12 @@
+package pl.mberkan.softwarecraftmanship.awsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsdemoApplication.class, args);
+	}
+}
