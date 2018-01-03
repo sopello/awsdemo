75993ac8a31a1528cd2a55b401f9c08209c7c59b
Marek Berkan <mberkan@e-point.pl>
2018-01-03 16:53:02 +0100

    Testowy commit to wymuszenia budowania w travis-ci.

--- src/main/java/pl/mberkan/softwarecraftmanship/awsdemo/AwsdemoApplication.java
+++ src/main/java/pl/mberkan/softwarecraftmanship/awsdemo/AwsdemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AwsdemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Startup test!");
 		SpringApplication.run(AwsdemoApplication.class, args);
 	}
 }
